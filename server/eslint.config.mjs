a8effa40787a9243f464493d8fc383c74e2e5a6c
kavishkasandaruwan2002 <kavishkasandaruwan608@gmail.com>
2026-03-05 16:18:55 +0530

    fixed some error

---
+++ server/eslint.config.mjs
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+    js.configs.recommended,
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: "commonjs",
+            globals: {
+                ...globals.node,
+            },
+        },
+        rules: {
+            "no-unused-vars": "warn",
+            "no-undef": "error",
+            "no-console": "off",
+        },
+    },
+];
